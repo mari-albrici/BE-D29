2d46eb82f12cb1ae56a80b4d2b1fe97c5a7175e3
Marianna <121226166+mari-albrici@users.noreply.github.com>
2023-06-15 16:54:08 +0200

    Main test for checker

--- es3/src/main/java/designPatterns/es3/Es3Application.java
+++ es3/src/main/java/designPatterns/es3/Es3Application.java
@@ -1,5 +1,6 @@
 package designPatterns.es3;
 
+import designPatterns.es3.handlers.RoleChecker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Es3Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Es3Application.class, args);
+
+		long salary = 3560;
+
+		RoleChecker roleChecker = new RoleChecker();
+
+		System.out.println(roleChecker.roleCheck(salary));
 	}
 
 }
--- es3/src/main/java/designPatterns/es3/handlers/RoleChecker.java
+++ es3/src/main/java/designPatterns/es3/handlers/RoleChecker.java
@@ -5,16 +5,16 @@ import designPatterns.es3.entities.enums.Role;
 public class RoleChecker {
     Role role;
     long salary;
-    SalaryValidator salaryValidator;
+    SalaryValidator salaryValidator = new SalaryValidator();
 
-    public Role roleCheck(){
-        if(salaryValidator.salaryCheck(salary) && salary>=1000){
+    public Role roleCheck(long salary){
+        if(salaryValidator.salaryCheck(salary) && salary>=1000 && salary<=1999){
             return Role.CAPTAIN;
-        } else if(salaryValidator.salaryCheck(salary) && salary>=2000){
+        } else if(salaryValidator.salaryCheck(salary) && salary>=2000 && salary<=2999){
             return Role.MAJOR;
-        } else if(salaryValidator.salaryCheck(salary) && salary>=3000){
+        } else if(salaryValidator.salaryCheck(salary) && salary>=3000 && salary<=3999){
             return Role.LIEUTENANT;
-        } else if(salaryValidator.salaryCheck(salary) && salary>=4000){
+        } else if(salaryValidator.salaryCheck(salary) && salary>=4000 && salary<=4999){
             return Role.COLONEL;
         } else if(salaryValidator.salaryCheck(salary) && salary>=5000){
             return Role.GENERAL;
